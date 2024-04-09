6601d12451b64fbc300e2d8e74a009a5d9d4eaec
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-04-09 11:28:51 -0500

    feat: create new social component to reuse inside of another and reduce lines

--- components/Social.jsx
+++ components/Social.jsx
@@ -1,23 +1,21 @@
-import Link from "next/link";
+import SocialLink from "./SocialLink";
 
 const Social = () => {
   return (
     <div className="text-gray-800 mt-28 pb-14 w-full text-center flex justify-center ls:mt-6 ls:pt-0 xl:mt-20 xl:pt-24 xl:pb-10">
       <div className="socials flex flex-col">
-        <Link href="https://www.linkedin.com/in/camilosc98/">
-          <a className="p-4 link flex flex-col transition-all duration-500" target="_blank">
-            <i className="fab fa-linkedin fa-3x icon"></i>
-            LinkedIn
-          </a>
-        </Link>
+        <SocialLink
+          url="https://www.linkedin.com/in/camilosc98/"
+          icon="fab fa-linkedin fa-3x icon"
+          socialName="LinkedIn"
+        />
       </div>
       <div className="flex flex-col">
-        <Link href="https://github.com/Camilo-Suarez98">
-          <a className="p-4 link flex flex-col transition-all duration-500" target="_blank">
-            <i className="fab fa-github fa-3x icon"></i>
-            GitHub
-          </a>
-        </Link>
+        <SocialLink
+          url="https://github.com/Camilo-Suarez98"
+          icon="fab fa-github fa-3x icon"
+          socialName="GitHub"
+        />
       </div>
     </div>
   );
---
+++ components/SocialLink.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const SocialLink = ({ url, icon, socialName }) => {
+  return (
+    <Link href={url}>
+      <a className="p-4 link flex flex-col transition-all duration-500" target="_blank">
+        <i className={icon}></i>
+        {socialName}
+      </a>
+    </Link>
+  )
+}
+
+export default SocialLink;
