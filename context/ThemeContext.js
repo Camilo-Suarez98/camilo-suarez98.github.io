053018b850f3fd23529e7824ff665de4c1f49670
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-04-09 11:40:12 -0500

    feat: include new css property and change theme color default

--- context/ThemeContext.js
+++ context/ThemeContext.js
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 const ThemeContext = createContext();
 
 function ThemeProvider({ children }) {
-  const [theme, setTheme] = useState('dark');
+  const [theme, setTheme] = useState('light');
   const toggleTheme = () => {
     setTheme(theme === 'light' ? 'dark' : 'light')
   };
