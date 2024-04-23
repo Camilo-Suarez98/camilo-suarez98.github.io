98e9c75287b533ab5bc2af7da2b34514627d0f50
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-04-23 10:57:07 -0500

    fix(chore): delete RemoveScroll component

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,7 +8,6 @@ import logo from "/public/icono-portf.png";
 import BurgerMenu from "./BurgerMenu";
 import useToggleMenu from "../utils/useToggleMenu";
 import useEscapeKey from "../utils/useEscapeKey";
-import { RemoveScroll } from "react-remove-scroll";
 
 const Header = () => {
   const { theme, toggleTheme } = useContext(ThemeContext);
@@ -31,36 +30,34 @@ const Header = () => {
 
         <div className="flex flex-grow items-center z-50 transition justify-end duration-500 sm:justify-center sm:text-center ls:w-full ls:absolute ls:top-12 ls:-bottom-72 ls:m-auto">
           <FocusLock returnFocus={true}>
-            <RemoveScroll>
-              <ul className={!value ? 'ls:hidden flex list-none ml-auto transition-all duration-300' : 'menu-burger fixed inset-0 p-4 list-none ml-auto transition-all duration-300 ls:flex-col sm:justify-start ls:w-full ls:m-auto ls:items-center'}>
-                <NavLink href="/" title="Home" />
-                <NavLink href="/portfolio" title="Projects" />
-                <NavLink href="/contact" title="Contact" />
-                <li className="w-full">
-                  <a
-                    href="https://drive.google.com/file/d/15h6n_X03FUTc6x5KbXa-MhcUPUVQ_lXg/view"
-                    target="_blank"
-                    className="border-resume px-3 py-2 border-2 sm:border-4 flex items-center text-sm uppercase font-bold leading-snug text-gray-800 transition duration-500 hover:text-blue-700 ls:w-full ls:py-5 ls:justify-center"
-                  >
-                    CV
-                  </a>
-                </li>
+            <ul className={!value ? 'ls:hidden flex list-none ml-auto transition-all duration-300' : 'menu-burger fixed inset-0 p-4 list-none ml-auto transition-all duration-300 ls:flex-col sm:justify-start ls:w-full ls:m-auto ls:items-center'}>
+              <NavLink href="/" title="Home" />
+              <NavLink href="/portfolio" title="Projects" />
+              <NavLink href="/contact" title="Contact" />
+              <li className="w-full">
+                <a
+                  href="https://drive.google.com/file/d/15h6n_X03FUTc6x5KbXa-MhcUPUVQ_lXg/view"
+                  target="_blank"
+                  className="border-resume px-3 py-2 border-2 sm:border-4 flex items-center text-sm uppercase font-bold leading-snug text-gray-800 transition duration-500 hover:text-blue-700 ls:w-full ls:py-5 ls:justify-center"
+                >
+                  CV
+                </a>
+              </li>
+              <li className="w-full px-3 py-2 flex justify-center items-center">
+                <button onClick={toggleTheme}>
+                  {theme === 'light' ? '🌝' : '🌚'}
+                </button>
+              </li>
+              {value &&
                 <li className="w-full px-3 py-2 flex justify-center items-center">
-                  <button onClick={toggleTheme}>
-                    {theme === 'light' ? '🌝' : '🌚'}
+                  <button
+                    className="close-btn flex items-center justify-center gap-2 p-2 cursor-pointer border-none rounded-md"
+                    onClick={toggleValue}>
+                    <span className="text-xl font-black">X</span> Dismiss
                   </button>
                 </li>
-                {value &&
-                  <li className="w-full px-3 py-2 flex justify-center items-center">
-                    <button
-                      className="close-btn flex items-center justify-center gap-2 p-2 cursor-pointer border-none rounded-md"
-                      onClick={toggleValue}>
-                      <span className="text-xl font-black">X</span> Dismiss
-                    </button>
-                  </li>
-                }
-              </ul>
-            </RemoveScroll>
+              }
+            </ul>
           </FocusLock>
         </div>
       </div>
