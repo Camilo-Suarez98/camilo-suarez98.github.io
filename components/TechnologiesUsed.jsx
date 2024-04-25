51123191880e19be771f70c550d7ca7a90dbe002
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-04-25 09:49:34 -0500

    fix(chore): include alt attribute to images

--- components/TechnologiesUsed.jsx
+++ components/TechnologiesUsed.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const TechnologiesUsed = ({ icon, technologyName }) => {
   return (
     <div className="flex flex-col items-center justify-center ls:py-4 transition-all duration-300 hover:-translate-y-3">
-      <Image src={icon} width={50} height={50} />
+      <Image src={icon} width={50} height={50} alt={technologyName} />
       <h3>{technologyName}</h3>
     </div>
   );
