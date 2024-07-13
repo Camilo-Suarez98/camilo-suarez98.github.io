24e42d288fbc284935db059d3e4bce917ea30ff9
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-07-13 11:48:08 -0500

    fix: change ids format

--- utils/projects.js
+++ utils/projects.js
@@ -1,6 +1,6 @@
 export const projects = [
   {
-    id: 1,
+    id: crypto.randomUUID(),
     img: '/project-images/pokedex.png',
     name: 'PokeDex',
     description: 'This is a project to show different characters from the famous Pokemon series, you can search by name or by id to see information about any Pokemon.',
@@ -9,7 +9,7 @@ export const projects = [
     repo: 'https://github.com/Camilo-Suarez98/PokeDex'
   },
   {
-    id: 2,
+    id: crypto.randomUUID(),
     img: '/project-images/playgreen.png',
     name: 'Playgreen Sports',
     description: "This is the 'Tinder' for sport, where you can like or dislike different sports and you can see your selection history, This project includes login, register and logout function using firebase.",
@@ -18,7 +18,7 @@ export const projects = [
     repo: 'https://github.com/Camilo-Suarez98/playgreen-sports'
   },
   {
-    id: 3,
+    id: crypto.randomUUID(),
     img: '/project-images/nft.png',
     name: 'NFT',
     description: "This is a project to create, sell and bid for nft's by auction. It's possible to create user and login with third party libraries like Google.",
@@ -27,7 +27,7 @@ export const projects = [
     repo: 'https://github.com/Camilo-Suarez98/nft-marketplace-frontend'
   },
   {
-    id: 4,
+    id: crypto.randomUUID(),
     img: '/project-images/rick-and-morty.png',
     name: 'Rick and Morty',
     description: "This is a project to see all chaacters from famous Rick and Morty series, also you can see all info about episodes and locations.",
@@ -36,7 +36,7 @@ export const projects = [
     repo: 'https://github.com/Camilo-Suarez98/rick-and-morty-graphql'
   },
   {
-    id: 5,
+    id: crypto.randomUUID(),
     img: '/project-images/tenis-league.png',
     name: 'Tennis League',
     description: "This is a project to create, update and register for different tennis tournaments. It's possible to login with third party libraries like Google.",
@@ -45,7 +45,7 @@ export const projects = [
     repo: 'https://github.com/Camilo-Suarez98/liga-de-tenis'
   },
   {
-    id: 6,
+    id: crypto.randomUUID(),
     img: '/project-images/exercise-results.png',
     name: 'VerifyFit',
     description: "This is a project to upload a file or manually enter data into a form on the walk result to determine if it is real or fake.",
@@ -54,7 +54,7 @@ export const projects = [
     repo: 'https://github.com/Camilo-Suarez98/excercise-results'
   },
   {
-    id: 7,
+    id: crypto.randomUUID(),
     img: '/project-images/automarket.png',
     name: 'Auto Market',
     description: "This is a project to create and search for cars for sale. It's possible to create user and login with third party libraries like Google.",
@@ -63,7 +63,7 @@ export const projects = [
     repo: 'https://github.com/Camilo-Suarez98/auto-market-frontend'
   },
   {
-    id: 8,
+    id: crypto.randomUUID(),
     img: '/project-images/table-crud.png',
     name: 'Table CRUD',
     description: "This is a project to create, read, update and delete elements. You can choose any element and update or delete it, or create a new one.",
