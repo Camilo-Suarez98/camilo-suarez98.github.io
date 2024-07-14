51908b9295779c959d120c2dbf3ab4f3a36af1dc
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-07-14 17:36:25 -0500

    feat: update nextjs version

--- components/SocialLink.jsx
+++ components/SocialLink.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const SocialLink = ({ url = '', icon, socialName }) => {
   return (
-    <Link href={url}>
+    <Link href={url} legacyBehavior>
       <a className="p-4 link flex flex-col transition-all duration-500" target="_blank">
         <i className={icon}></i>
         {socialName}
--- next.config.js
+++ next.config.js
@@ -4,8 +4,20 @@ module.exports = {
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
-        crypto: require.resolve('crypto-browserify')
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        assert: require.resolve('assert'),
+        http: require.resolve('stream-http'),
+        https: require.resolve('https-browserify'),
+        os: require.resolve('os-browserify/browser'),
+        url: require.resolve('url')
       };
+      config.plugins.push(
+        new webpack.ProvidePlugin({
+          process: 'process/browser',
+          Buffer: ['buffer', 'Buffer']
+        })
+      );
     }
     return config;
   },
