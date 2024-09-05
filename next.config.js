8846748f8fb55276ed4f440c3ab693538293cab3
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-09-05 16:03:28 -0500

    delete some packages installed and not neccesaries for the project

--- next.config.js
+++ next.config.js
@@ -1,24 +1,5 @@
-const webpack = require('webpack');
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-module.exports = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        crypto: require.resolve('crypto-browserify'),
-        stream: require.resolve('stream-browserify'),
-        assert: require.resolve('assert'),
-        http: require.resolve('stream-http'),
-        https: require.resolve('https-browserify'),
-        os: require.resolve('os-browserify/browser'),
-        url: require.resolve('url')
-      };
-      config.plugins.push(
-        new webpack.ProvidePlugin({
-          process: 'process/browser',
-          Buffer: ['buffer', 'Buffer']
-        })
-      );
-    }
-    return config;
-  },
-};
+module.exports = nextConfig;
