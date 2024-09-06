c9a38ca6baee1c6cd187bab2fff85805cc157246
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-09-06 17:05:39 -0500

    include new style in tech stack component

--- components/TechStack.jsx
+++ components/TechStack.jsx
@@ -1,66 +1,54 @@
 const TechStack = () => {
   return (
     <div className="flex justify-center items-center gap-8 flex-wrap">
-      <div className="projects flex flex-col place-items-center p-8 rounded-3xl">
+      <div className="tooltip projects flex flex-col place-items-center p-8 rounded-3xl" data-tip="TypeScript">
         <img className="w-48" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" />
-        <p className="text-xl">TypeScript</p>
       </div>
 
-      <div className="projects flex flex-col place-items-center p-8 rounded-3xl">
+      <div className="tooltip projects flex flex-col place-items-center p-8 rounded-3xl" data-tip="php">
         <img className="w-48" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/php/php-original.svg" />
-        <p className="text-xl">PHP</p>
       </div>
 
-      <div className="projects flex flex-col place-items-center p-8 rounded-3xl">
+      <div className="tooltip projects flex flex-col place-items-center p-8 rounded-3xl" data-tip="WordPress">
         <img className="w-48" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/wordpress/wordpress-plain.svg" />
-        <p className="text-xl">WordPress</p>
       </div>
 
-      <div className="projects flex flex-col place-items-center p-8 rounded-3xl">
+      <div className="tooltip projects flex flex-col place-items-center p-8 rounded-3xl" data-tip="React.js">
         <img className="w-48" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" />
-        <p className="text-xl">React.js</p>
       </div>
 
-      <div className="projects flex flex-col place-items-center p-8 rounded-3xl">
+      <div className="tooltip projects flex flex-col place-items-center p-8 rounded-3xl" data-tip="Next.js">
         <img className="w-48" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original-wordmark.svg" />
-        <p className="text-xl">Next.js</p>
       </div>
 
-      <div className="projects flex flex-col place-items-center p-8 rounded-3xl">
+      <div className="tooltip projects flex flex-col place-items-center p-8 rounded-3xl" data-tip="TailwindCSS">
         <img className="w-48" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" />
-        <p className="text-xl">TailwindCSS</p>
       </div>
 
-      <div className="projects flex flex-col place-items-center p-8 rounded-3xl">
+      <div className="tooltip projects flex flex-col place-items-center p-8 rounded-3xl" data-tip="Sass">
         <img className="w-48" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg" />
-        <p className="text-xl">Sass</p>
       </div>
 
-      <div className="projects flex flex-col place-items-center p-8 rounded-3xl">
+      <div className="tooltip projects flex flex-col place-items-center p-8 rounded-3xl" data-tip="Node.js">
         <img className="w-48" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg" />
-        <p className="text-xl">Node.js</p>
       </div>
 
-      <div className="projects flex flex-col place-items-center p-8 rounded-3xl">
+      <div className="tooltip projects flex flex-col place-items-center p-8 rounded-3xl" data-tip="Express">
         <img className="w-48" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg" />
-        <p className="text-xl">Express</p>
       </div>
 
-      <div className="projects flex flex-col place-items-center p-8 rounded-3xl">
+      <div className="tooltip projects flex flex-col place-items-center p-8 rounded-3xl" data-tip="MongoDB">
         <img className="w-48" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg" />
-        <p className="text-xl">MongoDB</p>
       </div>
 
-      <div className="projects flex flex-col place-items-center p-8 rounded-3xl">
+      <div className="tooltip projects flex flex-col place-items-center p-8 rounded-3xl" data-tip="PostgreSQL">
         <img className="w-48" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg" />
-        <p className="text-xl">PostgreSQL</p>
       </div>
 
-      <div className="projects flex flex-col place-items-center p-8 rounded-3xl">
+      <div className="tooltip projects flex flex-col place-items-center p-8 rounded-3xl" data-tip="GraphQL">
         <img className="w-48" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/graphql/graphql-plain.svg" />
-        <p className="text-xl">GraphQL</p>
       </div>
-    </div>
+    </div >
   )
 }
 
