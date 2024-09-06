e417ddd6c0a69681b5d5d5943515168bcc03148d
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2024-09-06 16:46:24 -0500

    include style to link button

--- components/SocialLink.jsx
+++ components/SocialLink.jsx
@@ -4,7 +4,7 @@ const SocialLink = ({ url = '', icon, hoverColor }) => {
   return (
     <Link
       href={url}
-      className={`${hoverColor} link px-4 flex flex-col transition-all duration-500`} target="_blank"
+      className={`${hoverColor} no-underline link px-4 flex flex-col transition-all duration-500`} target="_blank"
     >
       <i className={icon}></i>
     </Link>
